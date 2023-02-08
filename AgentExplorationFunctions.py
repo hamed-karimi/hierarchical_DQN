c88096347ff437cb58b9277df30644f3a3a8534f
hamed-karimi <hamedk72@yahoo.com>
2023-02-08 12:31:41 -0500

    bug fixed

--- AgentExplorationFunctions.py
+++ AgentExplorationFunctions.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 
 
 def agent_reached_goal(agent, environment, goal_index):
-    agent_object_maps_equality = torch.all(torch.all(torch.eq(environment.env_map[0, 0, :, :], environment.env_map[0, 1:, :, :]), dim=2), dim=1)
-    if torch.any(agent_object_maps_equality):
+    target_goal_layer = 0 if goal_index == environment.nObj else goal_index+1
+    agent_object_maps_equality = torch.all(torch.eq(environment.env_map[0, 0, :, :],
+                                                    environment.env_map[0, target_goal_layer, :, :]))
+    if agent_object_maps_equality:
         return True
     return False
     # if torch.all(torch.eq(agent.location, environment.object_locations[goal_index, :])):
