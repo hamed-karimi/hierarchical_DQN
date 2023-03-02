5225389d4c4886492fd0ea583d78a05fd3c9869e
hamed-karimi <hamedk72@yahoo.com>
2023-03-02 16:55:36 -0500

    reached goal strategy changed.

--- AgentExplorationFunctions.py
+++ AgentExplorationFunctions.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 
 def agent_reached_goal(agent, environment, goal_index):
-    target_goal_layer = 0 if goal_index == environment.nObj else goal_index+1
+    if goal_index == environment.nObj:
+        return False
+
+    target_goal_layer = goal_index+1
     agent_object_maps_equality = torch.all(torch.eq(environment.env_map[0, 0, :, :],
                                                     environment.env_map[0, target_goal_layer, :, :]))
     if agent_object_maps_equality:
         return True
     return False
-    # if torch.all(torch.eq(agent.location, environment.object_locations[goal_index, :])):
-    #     return True
-    # return False
 
 
 def get_controller_loss(controller_at_loss, device):
